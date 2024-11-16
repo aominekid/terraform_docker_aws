451280a990c51870fa21cb7c597e2a651bdd5b08
aominekid <adkid>
2024-11-16 15:59:29 +0100

    added subnet on the vpc

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,13 @@ resource "aws_vpc" "vpc_docker" {
     Name = "vpc-docker"
   }
 }
+
+# Subnetz erstellen
+resource "aws_subnet" "docker_subnet" {
+  vpc_id            = aws_vpc.vpc_docker.id
+  cidr_block        = "10.0.0.0/28"
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "docker-subnet"
+  }
+}
